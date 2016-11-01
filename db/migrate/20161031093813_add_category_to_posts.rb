ce8242084baf41b9349e6b783af7321ac8be7565
Sebastian Peterlin <speterlin@gmail.com>
2016-11-01 23:53:43 +0100

    Need to add category to posts first, then add not null attribute

--- db/migrate/20161031093813_add_category_to_posts.rb
+++ db/migrate/20161031093813_add_category_to_posts.rb
@@ -1,6 +1,5 @@
 class AddCategoryToPosts < ActiveRecord::Migration[5.0]
   def change
     add_column :posts, :category, :string
-    change_column :posts, :category, :string, null: false
   end
 end
