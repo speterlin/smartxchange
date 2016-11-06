1833802b37e9f64bf3a7839fb60fe0019e618a21
Sebastian Peterlin <speterlin@gmail.com>
2016-11-06 23:30:55 +0100

    Add || Rails.env.test? to application.rb so environment variables are loaded in test and development environments.

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require 'omniauth'
 Bundler.require(*Rails.groups)
 
 # preload tokens in application.yml to local ENV
-if Rails.env.development?
+if Rails.env.development? || Rails.env.test?
   config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
   config.merge! config.fetch(Rails.env, {})
   config.each do |key, value|
