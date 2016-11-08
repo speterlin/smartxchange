16fcf8423829575689589d57ccfce1ac666a4372
Sebastian Peterlin <speterlin@gmail.com>
2016-11-08 13:18:19 +0100

    Edit and add more board controller and chat_rooms controller tests, add comments, packages, users (person of interest, chatbot and tutor) fixtures.

--- test/controllers/boards_controller_test.rb
+++ test/controllers/boards_controller_test.rb
@@ -18,7 +18,7 @@ class BoardsControllerTest < ActionDispatch::IntegrationTest
     assert User.third.posts_notifications.count == 1
   end
 
-  test "when a follower comments on a post he/she is not notified" do
+  test "when a follower comments on a post the post owner and followers are notified but he/she is not notified" do
     post "/session", :user => {:email => User.third.email, :password => 'password'}
     assert_difference('Notification.count', 2) do
       post "/comments", params: { comment: { content: 'example comment', commentable_type: 'Post', commentable_id: @post.id} }
@@ -28,7 +28,7 @@ class BoardsControllerTest < ActionDispatch::IntegrationTest
     assert_not User.third.posts_notifications.count == 1
   end
 
-  test "when an owner comments on a post he/she is not notified" do
+  test "when an owner comments on a post he/she is not notified but followers are notified" do
     post "/session", :user => {:email => User.first.email, :password => 'password'}
     assert_difference('Notification.count', 2) do
       post "/comments", params: { comment: { content: 'example comment', commentable_type: 'Post', commentable_id: @post.id} }
@@ -49,9 +49,14 @@ class BoardsControllerTest < ActionDispatch::IntegrationTest
     assert User.fourth.followed_posts.include?(@post)
   end
 
-  test "when there is a new downvote person is removed as follower" do
+  test "when there is a new downvote post owner and followers are notified and person is removed as follower" do
     post "/session", :user => {:email => User.third.email, :password => 'password'}
-    post "/posts/#{@post.id}/downvote"
+    assert_difference('Notification.count', 2) do
+      post "/posts/#{@post.id}/downvote"
+    end
+    assert User.first.posts_notifications.count == 1
+    assert User.second.posts_notifications.count == 1
+    assert_not User.third.posts_notifications.count == 1
     assert_not User.third.followed_posts.include?(@post)
   end
 
@@ -64,6 +69,85 @@ class BoardsControllerTest < ActionDispatch::IntegrationTest
     assert User.first.posts_notifications.count == 1
     assert User.second.posts_notifications.count == 1
     assert User.third.posts_notifications.count == 1
+  end
+
+  test "when there is a new follow request post owner and followers are notified" do
+    post "/session", :user => {:email => User.fourth.email, :password => 'password'}
+    assert_difference('Notification.count', 3) do
+      post "/posts/#{@post.id}/follow"
+    end
+    assert User.first.posts_notifications.count == 1
+    assert User.second.posts_notifications.count == 1
+    assert User.third.posts_notifications.count == 1
+  end
+
+  test "when there is an unfollow request post owner and followers are notified" do
+    post "/session", :user => {:email => User.third.email, :password => 'password'}
+    assert_difference('Notification.count', 2) do
+      post "/posts/#{@post.id}/unfollow"
+    end
+    assert User.first.posts_notifications.count == 1
+    assert User.second.posts_notifications.count == 1
+  end
+
+  test "when there is a post update followers are notified but not post owner" do
+    post "/session", :user => {:email => User.first.email, :password => 'password'}
+    assert_difference('Notification.count', 2) do
+      patch "/posts/#{@post.id}", :post => {content: "edit post"}
+    end
+    assert_not User.first.posts_notifications.count == 1
+    assert User.second.posts_notifications.count == 1
+    assert User.third.posts_notifications.count == 1
+  end
+
+  test "only allow post owner to update post" do
+    post "/session", :user => {:email => User.second.email, :password => 'password'}
+    patch "/posts/#{@post.id}", :post => {content: "edit post"}
+    assert_redirected_to root_path
+    assert_equal flash[:error], "Unauthorized access"
+  end
+
+  test "only allow comment owner to update comment" do
+    post "/session", :user => {:email => User.second.email, :password => 'password'}
+    patch "/comments/#{Comment.first.id}", :comment => {content: "edit comment"}
+    assert_redirected_to root_path
+    assert_equal flash[:error], "Unauthorized access"
+  end
+
+  test "only allowed to post 5 times" do
+    post "/session", :user => {:email => User.second.email, :password => 'password'}
+    assert_difference('Post.count', 5) do
+      5.times do
+        post "/posts", post: {content: 'example', board_id: 1, category: 'Interest'}
+      end
+    end
+    assert_difference('Post.count', 0) do
+      post "/posts", post: {content: 'example', board_id: 1, category: 'Interest'}
+    end
+  end
+
+  test "only allowed to vote 10 times" do
+    post "/session", :user => {:email => User.first.email, :password => 'password'}
+    assert_difference('Vote.count', 10) do
+      10.times do
+        post "/posts/#{@post.id}/upvote"
+      end
+    end
+    assert_difference('Vote.count', 0) do
+      post "/posts/#{@post.id}/upvote"
+    end
+  end
+
+  test "only allowed to comment 10 times" do
+    post "/session", :user => {:email => User.second.email, :password => 'password'}
+    assert_difference('Comment.count', 10) do
+      10.times do
+        post "/comments", :comment => {content: "new comment", commentable_type: Post, commentable_id: 1}
+      end
+    end
+    assert_difference('Comment.count', 0) do
+      post "/comments", :comment => {content: "new comment", commentable_type: Post, commentable_id: 1}
+    end
   end
 
   test "when there is an unread board board header color changes to yellow" do
--- test/controllers/chat_rooms_controller_test.rb
+++ test/controllers/chat_rooms_controller_test.rb
@@ -1,7 +1,62 @@
 require 'test_helper'
 
 class ChatRoomsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    # make first user premium
+    User.first.package = Package.second
+  end
+
+  test "can not create new chat room with person of interest for a regular member" do
+    post "/session", :user => {:email => User.second.email, :password => 'password'}
+    assert_difference('ChatRoom.count', 0) do
+      post "/chat_rooms", chat_room: {recipient_id: 5}
+    end
+    assert_equal flash[:error], "Recipient is a Person of Interest, Chatbot, or Tutor, Premium membership required"
+  end
+
+  test "can not create new chat room with tutor for a regular member" do
+    post "/session", :user => {:email => User.second.email, :password => 'password'}
+    assert_difference('ChatRoom.count', 0) do
+      post "/chat_rooms", chat_room: {recipient_id: 7}
+    end
+    assert_equal flash[:error], "Recipient is a Person of Interest, Chatbot, or Tutor, Premium membership required"
+  end
+
+  test "can not create new chat room with chatbot for a regular member" do
+    post "/session", :user => {:email => User.second.email, :password => 'password'}
+    assert_difference('ChatRoom.count', 0) do
+      post "/chat_rooms", chat_room: {recipient_id: 6}
+    end
+    assert_equal flash[:error], "Recipient is a Person of Interest, Chatbot, or Tutor, Premium membership required"
+  end
+
+  test "premium member can create chat room with person of interest or chatbot or tutor" do
+    post "/session", :user => {:email => User.first.email, :password => 'password'}
+    assert_difference('ChatRoom.count', 1) do
+      post "/chat_rooms", chat_room: {recipient_id: 5}
+    end
+    assert_difference('ChatRoom.count', 1) do
+      post "/chat_rooms", chat_room: {recipient_id: 6}
+    end
+    assert_difference('ChatRoom.count', 1) do
+      post "/chat_rooms", chat_room: {recipient_id: 7}
+    end
+  end
+
+  test "can only access chat room that you are involved in" do
+    post "/session", :user => {:email => User.third.email, :password => 'password'}
+    get "/chat_rooms/#{ChatRoom.first.id}"
+    assert_redirected_to users_path
+    assert_equal flash[:error], "Unauthorized access"
+  end
+
+  test "if chat room already exists between you and recipient with title you are redirected to the that chat room" do
+    post "/session", :user => {:email => User.first.email, :password => 'password'}
+    assert_difference('ChatRoom.count', 0) do
+      post "/chat_rooms", chat_room: {recipient_id: 2}
+    end
+    assert_select "div#messages", data: {chat_room_id: 1}
+  end
+
 end
