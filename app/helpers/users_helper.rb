bbb7240536a9318900d6a957b2f3f4509d916607
Sebastian Peterlin <speterlin@gmail.com>
2017-02-01 06:17:19 +0100

    Refactoring: Add nationality 2d array and simplify interest display users_helper.rb: add #user_nationalities method which returns nationalities array. Acts as single place for user nationalities to display in users/new&show.html.erb, also edit #user_convert_interests(number) to evaluate integer number instead of string number. users/new.html.erb: Remove list of nationalities, replace with loop which iterates over array returned from users_helper.rb#user_nationalities. users/show.html.erb: Remove list of nationalities and replace with loop, replace user interest display with nested loop which retrieves the correct interest from users_helper.rb, also add a to_s and to_i conversion to interact with the database since database stores array with numbers as strings.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -82,66 +82,151 @@ module UsersHelper
     end
   end
 
+  def user_nationalities
+    # maybe refactor and make object
+    [
+      ["Algerian", "Algeria"],
+      ["Armenian", "Armenia"],
+      ["Argentinian", "Argentina"],
+      ["Australian", "Australia"],
+      ["Austrian", "Austria"],
+      ["Azerbaijani", "Azerbaijan"],
+      ["Bengali", "Bangladesh"],
+      ["Belgian", "Belgium"],
+      ["Bolivian", "Bolivia"],
+      ["Brazilian", "Brazil"],
+      ["Bulgarian", "Bulgaria"],
+      ["Cambodian", "Cambodia"],
+      ["Canadian", "Canada"],
+      ["Chilean", "Chile"],
+      ["Chinese", "China"],
+      ["Colombian", "Colombia"],
+      ["Croatian", "Croatia"],
+      ["Cypriot", "Cyprus"],
+      ["Czech", "Czech Republic"],
+      ["Danish", "Denmark"],
+      ["Dutch", "Netherlands"],
+      ["Ecuadorian", "Ecuador"],
+      ["Estonian", "Estonia"],
+      ["Ethiopian", "Ethiopia"],
+      ["Filipino", "Philippines"],
+      ["Finnish", "Finland"],
+      ["French", "France"],
+      ["Georgian", "Georgia"],
+      ["German", "Germany"],
+      ["Guatemalan", "Guatemala"],
+      ["Greek", "Greece"],
+      ["Honduran", "Honduras"],
+      ["Hungarian", "Hungary"],
+      ["Icelandic", "Iceland"],
+      ["Indian", "India"],
+      ["Indonesian", "Indonesia"],
+      ["Iranian", "Iran"],
+      ["Iraqi", "Iraq"],
+      ["Irish", "Ireland"],
+      ["Israeli", "Israel"],
+      ["Italian", "Italy"],
+      ["Japanese", "Japan"],
+      ["Jordanian", "Jordan"],
+      ["Kazakhstani", "Kazakhstan"],
+      ["Kenyan", "Kenya"],
+      ["Latvian", "Latvia"],
+      ["Lebanese", "Lebanon"],
+      ["Lithuanian", "Lithuania"],
+      ["Luxembourgish", "Luxembourg"],
+      ["Malaysian", "Malaysia"],
+      ["Maltan", "Malta"],
+      ["Mexican", "Mexico"],
+      ["Moroccan", "Morocco"],
+      ["New Zealander", "New Zealand"],
+      ["Nigerian", "Nigeria"],
+      ["Omanian", "Oman"],
+      ["Paraguayan", "Paraguay"],
+      ["Peruvian", "Peru"],
+      ["Philippino", "Philippines"],
+      ["Polish", "Poland"],
+      ["Portuguese", "Portugal"],
+      ["Romanian", "Romania"],
+      ["Russian", "Russia"],
+      ["Saudi Arabian", "Saudi Arabia"],
+      ["Singaporean", "Singapore"],
+      ["Slovakian", "Slovakia"],
+      ["Slovenian", "Slovenia"],
+      ["South African", "South Africa"],
+      ["South Korean", "South Korea"],
+      ["Spanish", "Spain"],
+      ["Swedish", "Sweden"],
+      ["Thai", "Thailand"],
+      ["Turkish", "Turkey"],
+      ["Emirati", "United Arab Emirates"],
+      ["British", "United Kingdom"],
+      ["Uruguayan", "Uruguay"],
+      ["USA", "U.S.A"],
+      ["Venezuelan", "Venezuela"],
+      ["Vietnamese", "Vietnam"]
+    ]
+  end
+
   def user_convert_interests(number)
-    if number == "1"
+    if number == 1
       return "Sales / Business Development"
-    elsif number == "2"
+    elsif number == 2
       return "Marketing"
-    elsif number == "3"
+    elsif number == 3
       return "Engineering"
-    elsif number == "4"
+    elsif number == 4
       return "Web Development / Design"
-    elsif number == "5"
+    elsif number == 5
       return "Finance"
-    elsif number == "6"
+    elsif number == 6
       return "Law"
-    elsif number == "7"
+    elsif number == 7
       return "Project Management"
-    elsif number == "8"
+    elsif number == 8
       return "Research"
-    elsif number == "9"
+    elsif number == 9
       return "Consulting"
-    elsif number == "10"
+    elsif number == 10
       return "Medicine"
-    elsif number == "11"
+    elsif number == 11
       return "Start-ups"
-    elsif number == "12"
+    elsif number == 12
       return "Financial Services"
-    elsif number == "13"
+    elsif number == 13
       return "Pharma"
-    elsif number == "14"
+    elsif number == 14
       return "Architecture"
-    elsif number == "15"
+    elsif number == 15
       return "Computer Software"
-    elsif number == "16"
+    elsif number == 16
       return "Education"
-    elsif number == "17"
+    elsif number == 17
       return "Fashion"
-    elsif number == "18"
+    elsif number == 18
       return "Literature"
-    elsif number == "19"
+    elsif number == 19
       return "Music"
-    elsif number == "20"
+    elsif number == 20
       return "Dating"
-    elsif number == "21"
+    elsif number == 21
       return "Futbol / Soccer"
-    elsif number == "22"
+    elsif number == 22
       return "Basketball"
-    elsif number == "23"
+    elsif number == 23
       return "Rugby"
-    elsif number == "24"
+    elsif number == 24
       return "Tennis"
-    elsif number == "25"
+    elsif number == 25
       return "Padel"
-    elsif number == "26"
+    elsif number == 26
       return "Table Tennis"
-    elsif number == "27"
+    elsif number == 27
       return "Olympic Sports"
-    elsif number == "28"
+    elsif number == 28
       return "American Football"
-    elsif number == "29"
+    elsif number == 29
       return "Beer Pong"
-    elsif number == "30"
+    elsif number == 30
       return "Flip Cup"
     end
   end
