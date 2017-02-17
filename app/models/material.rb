dc3abfbf7e137d68c34ad7b01cba411933ab4248
Sebastian Peterlin <speterlin@gmail.com>
2017-02-17 13:27:59 -0800

    Add back attachment_uploader.rb and references in material.rb, this is not cause of heroku problem

--- app/models/material.rb
+++ app/models/material.rb
@@ -13,15 +13,15 @@
 class Material < ApplicationRecord
   validates :name, :attachment, :owner, presence: true
   validates :owner_id, uniqueness: { scope: :name, message: "has already uploaded a document with this name" }
-  # validate :attachment_is_unique_to_owner
+  validate :attachment_is_unique_to_owner
 
   belongs_to :owner, class_name: 'User'
-  # mount_uploader :attachment, AttachmentUploader
+  mount_uploader :attachment, AttachmentUploader
 
-  # def attachment_is_unique_to_owner
-  #   if self.attachment.file && Material.where(attachment: self.attachment.file.original_filename, owner_id: self.owner_id).where.not(id: self.id).count > 0
-  #      errors.add :owner_id, "has already uploaded an attachment with the name #{self.attachment.file.original_filename}"
-  #   end
-  # end
+  def attachment_is_unique_to_owner
+    if self.attachment.file && Material.where(attachment: self.attachment.file.original_filename, owner_id: self.owner_id).where.not(id: self.id).count > 0
+       errors.add :owner_id, "has already uploaded an attachment with the name #{self.attachment.file.original_filename}"
+    end
+  end
 
 end
