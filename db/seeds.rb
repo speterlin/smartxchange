a0b03b7a78bb75f8f0be2ae069c5d94614b2b94c
Sebastian Peterlin <speterlin@gmail.com>
2017-02-24 11:07:05 -0800

    Update seeds.rb second Package description to include 'tutor materials' and 'language related jobs listed on our platform'

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ b5 = Board.create!(title: 'German', description: 'A board for those looking to l
 b6 = Board.create!(title: 'French', description: 'A board for those looking to learn and practice French. A board where you can post about potential meetups, tutoring offers, professional tips, your projects (such as Kickstarter projects), links to your work (such as art, design, development), and educational, interesting, or other material. You can upvote or downvote a post (making someone\'s or your own post more popular), comment on a post, and follow a post so that you receive all notifications for that post. You are limited to 5 posts, 10 votes, and 10 comments per 24 hour period :).');
 
 p1 = Package.create!(classification: 'Standard', description: 'Access to the Platform', price: 0)
-p2 = Package.create!(classification: 'Premium', description: 'Access to conversations with persons of interest (Professors, Managers, CEOs, artists, etc), language learning aids (chatbots, smartXchange verified tutors), and tailored services to meet your needs!', price: 4.99)
+p2 = Package.create!(classification: 'Premium', description: 'Access to conversations with persons of interest (Professors, Managers, CEOs, artists, etc), language learning aids (chatbots, smartXchange verified tutors, tutor materials), language related jobs listed on our platform, and tailored services to meet your needs!', price: 4.99)
 # languages = ["Spanish","English","German","French"]
 # language_levels = [1,2,3,4,5,6,7,8,9,10]
 # ages = [23,25,27,29,31,33,35,37]
