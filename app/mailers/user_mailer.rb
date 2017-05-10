7c5ed08c93edd59077e41dbcb792156a89b4886d
Sebastian Peterlin <speterlin@gmail.com>
2017-05-09 22:03:04 -0700

    Prepare text for monthly_update.html&text.erb, correct title in user_mailer.rb#monthly_update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -45,7 +45,7 @@ class UserMailer < ApplicationMailer
     @notifications = notifications
     add_campaign_to_login(notifications_campaign)
     add_campaign_to_footer(notifications_campaign)
-    set_name_and_title_and_unsubscribe_and_header(@user, "smartXchange introduces Mandarin Chinese!")
+    set_name_and_title_and_unsubscribe_and_header(@user, "smartXchange enters its 10th month!")
   end
 
   def language_matches(user, match_or_exchange)
