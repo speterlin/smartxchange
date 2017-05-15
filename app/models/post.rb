859e130a4af33a4a95c49a711ba8170d7c7cad16
Sebastian Peterlin <speterlin@gmail.com>
2017-05-14 23:27:44 -0700

    Alter post.rb url format validation to allow http(s)://example.com, before http(s)://www... was required, also add back some of original validation from third party site (nearly identical except I require http(s):// in my format)

--- app/models/post.rb
+++ app/models/post.rb
@@ -22,8 +22,8 @@ class Post < ApplicationRecord
   validates :content, length: {minimum: 5, maximum: 500}
   validates :category, inclusion: {in: ["Jobs-Offered", "Jobs-Wanted", "Interest", "Educational", "Tutoring", "Meetup", "Professional", "Other"]}
   validates_uniqueness_of :url, scope: :board_id, unless: 'url.blank?'
-  # maybe refactor returns 2 match groups
-  validates :url, length: {maximum: 255 }, format: {:with => /(^https?\:\/\/www\.([-a-z0-9]+\.)+[-a-z0-9]+.*)/i}, if: 'url.present?'
+  # maybe refactor returns 2 match groups, alternative: (^https?\:\/\/www\.([-a-z0-9]+\.)+[-a-z0-9]+.*), taken from this site: https://forums.asp.net/t/1761988.aspx?Regular+expression+for+Validating+URL+with+or+without+http
+  validates :url, length: {maximum: 255 }, format: {:with => /(^(https?\:\/\/|www\.)(?:[-a-z0-9]+\.)*[-a-z0-9]+.*)/i}, if: 'url.present?'
 
   belongs_to :owner, class_name: 'User'
   belongs_to :board, touch: true
