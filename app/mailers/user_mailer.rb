a8afd328079a432ae8afc51c7a37caa32b315750
Sebastian Peterlin <speterlin@gmail.com>
2017-06-05 00:12:59 -0700

    Prepare text in monthly_update.text&html.erb and title in user_mailer.rb for sending. Minor refactoring: Updated README.md to reflect 300 users globally instead of previously 200.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -45,7 +45,7 @@ class UserMailer < ApplicationMailer
     @notifications = notifications
     add_campaign_to_login(notifications_campaign)
     add_campaign_to_footer(notifications_campaign)
-    set_name_and_title_and_unsubscribe_and_header(@user, "smartXchange enters its 10th month!")
+    set_name_and_title_and_unsubscribe_and_header(@user, "smartXchange enters its 11th month!")
   end
 
   def language_matches(user, match_or_exchange)
