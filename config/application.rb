894bb9447bd02d3d308eefdb28f9f4561a2bf624
Sebastian Peterlin <sebastianpeterlin@Sebastians-MacBook-Pro.local>
2023-05-13 23:22:02 -0700

    Change YAML.load to YAML.safe_load in config/application.rb to resolve precompiling assets issue: Psych::AliasesNotEnabled: Alias parsing was not enabled. To enable it, pass  to  or

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 
 # preload tokens in application.yml to local ENV
 if Rails.env.development? || Rails.env.test?
-  config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+  config = YAML.safe_load(File.read(File.expand_path('../application.yml', __FILE__)))
   config.merge! config.fetch(Rails.env, {})
   config.each do |key, value|
     ENV[key] = value.to_s unless value.kind_of? Hash
