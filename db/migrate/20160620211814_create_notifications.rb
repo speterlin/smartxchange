e38cc10a8c0c10cd9fb9bcad3ece76f35c891f78
Sebastian Peterlin <sebastianpeterlin@Sebastians-MacBook-Air.local>
2025-05-12 01:23:14 -0700

    Debug heroku-24 application: during ‘heroku run rake db:migrate -a smartexchange-heroku-24’, ‘StandardError: An error has occurred, this and all later migrations canceled: (StandardError) PG::UndefinedTable: ERROR:  relation \”subscribed_users\” does not exist’, commented out references to :subscribed_user in db/migrate/20160620211814_create_notifications.rb (references it and adds foreign key) and db/migrate/20160620235128_change_columns_to_notifications.rb (removes reference)

--- db/migrate/20160620211814_create_notifications.rb
+++ db/migrate/20160620211814_create_notifications.rb
@@ -2,7 +2,7 @@ class CreateNotifications < ActiveRecord::Migration[7.2]
   def change
     create_table :notifications do |t|
       t.references :user, index: true, foreign_key: true
-      t.references :subscribed_user, index: true, foreign_key: true
+      # t.references :subscribed_user, index: true, foreign_key: true
       t.references :chat, index: true, foreign_key: true
       t.integer :identifier #id of the message
       t.boolean :read
@@ -10,7 +10,7 @@ class CreateNotifications < ActiveRecord::Migration[7.2]
       t.timestamps null: false
     end
     add_foreign_key :notifications, :users
-    add_foreign_key :notifications, :users, column: :subscribed_user_id
+    # add_foreign_key :notifications, :users, column: :subscribed_user_id
     add_foreign_key :notifications, :chats
   end
 end
--- db/migrate/20160620235128_change_columns_to_notifications.rb
+++ db/migrate/20160620235128_change_columns_to_notifications.rb
@@ -1,7 +1,7 @@
 class ChangeColumnsToNotifications < ActiveRecord::Migration[7.2]
   def change
     remove_reference :notifications, :user
-    remove_reference :notifications, :subscribed_user
+    # remove_reference :notifications, :subscribed_user
     add_reference :notifications, :notified, index: true, foreign_key: true
     add_reference :notifications, :notifier, index: true, foreign_key: true
   end
