31f854d02d0edaa838596c68130fc14678d732fa
Sebastian Peterlin <sebastianpeterlin@Sebastians-MacBook-Air.local>
2025-05-13 04:04:08 -0700

    StandardError 'Unknown key: :index' in add_column :messages, :sender_id, :integer, index: true, foreign_key: true line in db/migrate/20160620233421_change_user_column_to_messages.rb, seperated into 3 calls - add_column :messages, :sender_id, :integer; add_index :messages, :sender_id; add_foreign_key :messages, :users, column: :sender_id

--- db/migrate/20160620233421_change_user_column_to_messages.rb
+++ db/migrate/20160620233421_change_user_column_to_messages.rb
@@ -1,6 +1,9 @@
 class ChangeUserColumnToMessages < ActiveRecord::Migration[7.2]
   def change
     remove_column :messages, :user_id
-    add_column :messages, :sender_id, :integer, index: true, foreign_key: true
+    # add_column :messages, :sender_id, :integer, index: true, foreign_key: true
+    add_column :messages, :sender_id, :integer
+    add_index :messages, :sender_id
+    add_foreign_key :messages, :users, column: :sender_id
   end
 end
